088f312af630cfd09a385171f77bf673c7cecc60
tapphughesn <nicholas.tapphughes@gmail.com>
2026-05-02 11:22:50 -0400

    ch 17.3

---
+++ async-runtime/src/main.rs
@@ -0,0 +1,49 @@
+extern crate trpl;
+use std::{thread, time::Duration};
+
+use trpl::Either;
+
+fn main() {
+    trpl::block_on(async {
+        let a = async {
+            println!("'a' started.");
+            slow("a", 30);
+            trpl::yield_now().await;
+            slow("a", 10);
+            trpl::yield_now().await;
+            slow("a", 20);
+            trpl::yield_now().await;
+            println!("'a' finished.")
+        };
+
+        let b = async {
+            println!("'b' started.");
+            slow("b", 75);
+            trpl::yield_now().await;
+            slow("b", 10);
+            trpl::yield_now().await;
+            slow("b", 15);
+            trpl::yield_now().await;
+            slow("b", 350);
+            trpl::yield_now().await;
+            println!("'b' finished.")
+        };
+
+        trpl::select(a, b).await;
+    })  
+}
+
+fn slow(name: &str, ms:u64) {
+    thread::sleep(Duration::from_millis(ms));
+    println!("'{name}' ran for {ms}ms");
+}
+
+async fn timeout<F: Future>(
+    future_to_try: F,
+    max_time: Duration,
+) -> Result<F::Output, Duration> {
+    match trpl::select(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }
+}
